f4f068344a21db02f68b91fe9c8386379a7bdd45
Narongrit Kanhanoi <narongrit@3dsinteractive.com>
2019-09-26 12:37:53 +0700

    first commit

---
+++ PamSdkTests/PamSdkTests.swift
@@ -0,0 +1,34 @@
+//
+//  PamSdkTests.swift
+//  PamSdkTests
+//
+//  Created by narongrit kanhanoi on 26/9/2562 BE.
+//  Copyright © 2562 PAM. All rights reserved.
+//
+
+import XCTest
+@testable import PamSdk
+
+class PamSdkTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
