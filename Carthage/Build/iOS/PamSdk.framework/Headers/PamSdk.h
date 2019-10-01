5b3afde7000dd697681eab36a5b567f2a227fff3
Narongrit Kanhanoi <narongrit@3dsinteractive.com>
2019-10-01 12:21:26 +0700

    Build 1.0.0

---
+++ Carthage/Build/iOS/PamSdk.framework/Headers/PamSdk.h
@@ -0,0 +1,19 @@
+//
+//  PamSdk.h
+//  PamSdk
+//
+//  Created by narongrit kanhanoi on 26/9/2562 BE.
+//  Copyright © 2562 PAM. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PamSdk.
+FOUNDATION_EXPORT double PamSdkVersionNumber;
+
+//! Project version string for PamSdk.
+FOUNDATION_EXPORT const unsigned char PamSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PamSdk/PublicHeader.h>
+
+
